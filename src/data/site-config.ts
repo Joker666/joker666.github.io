f83ba22102bd442590b381f6ae8ac269f4030aca
Rafi <mr.k779@outlook.com>
2024-11-21 23:45:48 -0400

    Update site config

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,12 +38,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-  title: "Dante",
-  subtitle: "Minimal Astro.js theme",
+  title: "Rafi",
+  subtitle: "Software engineer, indie hacker, and amateur traveller.",
   description:
     "Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com",
   image: {
-    src: "/dante-preview.jpg",
+    src: "/portrait.jpg",
     alt: "Dante - Astro.js and Tailwind CSS theme",
   },
   headerNavLinks: [
@@ -93,10 +93,10 @@ const siteConfig: SiteConfig = {
     },
   ],
   hero: {
-    title: "Hi There & Welcome to My Corner of the Web!",
-    text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+    title: "sudo welcome-to-my-world",
+    text: "I'm **Rafi**, a software engineer and indie hacker based in Canada. I'm currently working for Slumber Group as a senior software engineer. Right now I'm hacking some side projects, learning photography, and doing masters in machine learning. Explore my <a href='https://github.com/Joker666'>GitHub</a> or follow me on <a href='https://x.com/rafi_talks'>Twitter/X</a>.",
     image: {
-      src: "/hero.jpeg",
+      src: "/portrait.jpg",
       alt: "A person sitting at a desk in front of a computer",
     },
     actions: [
