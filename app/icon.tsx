58f1f4a7974c9b7d14dcb3711ddea0b264c44ec5
Rafi <mr.k779@outlook.com>
2025-12-30 11:23:29 -0500

    Icon

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,40 +1,38 @@
-import { ImageResponse } from 'next/og'
- 
+import { ImageResponse } from "next/og";
+
 // Image metadata
 export const size = {
   width: 256,
   height: 256,
-}
-export const contentType = 'image/png'
-export const dynamic = 'force-static'
- 
+};
+export const contentType = "image/png";
+export const dynamic = "force-static";
+
 // Image generation
 export default function Icon() {
   return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 160,
-          background: '#101010',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          color: '#f97316',
-          borderRadius: 0, // Brutalist style
-          fontWeight: 900,
-        }}
-      >
-        J
-      </div>
-    ),
+    // ImageResponse JSX element
+    <div
+      style={{
+        fontSize: 160,
+        background: "#101010",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#f97316",
+        borderRadius: 0, // Brutalist style
+        fontWeight: 900,
+      }}
+    >
+      R
+    </div>,
     // ImageResponse options
     {
       // For convenience, we can re-use the exported icons size metadata
       // config to also set the ImageResponse's width and height.
       ...size,
-    }
-  )
+    },
+  );
 }
