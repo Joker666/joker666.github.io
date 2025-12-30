0838fd6b0423aedb30178a384eea4470fccdae00
Rafi <mr.k779@outlook.com>
2025-12-30 02:27:32 -0500

    Blog setup

--- source.config.ts
+++ source.config.ts
@@ -1,14 +1,10 @@
-import {
-  defineConfig,
-  defineDocs,
-  frontmatterSchema,
-  metaSchema,
-} from 'fumadocs-mdx/config';
+import { defineConfig, defineDocs, defineCollections, frontmatterSchema, metaSchema } from "fumadocs-mdx/config";
+import { z } from "zod";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
   docs: {
     schema: frontmatterSchema,
     postprocess: {
@@ -18,6 +14,30 @@ export const docs = defineDocs({
   meta: {
     schema: metaSchema,
   },
+});
+
+export const blogs = defineCollections({
+  type: "doc",
+  dir: "content/blogs",
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z
+      .string()
+      .or(z.date())
+      .transform((value, context) => {
+        try {
+          return new Date(value);
+        } catch {
+          context.addIssue("Invalid date");
+          return z.NEVER;
+        }
+      }),
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional(),
+    draft: z.boolean().optional().default(false),
+    series: z.string().optional(),
+    seriesPart: z.number().optional(),
+  }),
 });
 
 export default defineConfig({
