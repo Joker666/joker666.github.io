4814553267fe527280853316570988f769fa9add
Rafi <mr.k779@outlook.com>
2025-12-30 11:29:12 -0500

    Update icon.tsx

--- app/icon.tsx
+++ app/icon.tsx
@@ -15,7 +15,7 @@ export default function Icon() {
     <div
       style={{
         fontSize: 160,
-        background: "#101010",
+        background: "#fffcf0",
         width: "100%",
         height: "100%",
         display: "flex",
