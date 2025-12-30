ea17902ee7c0c4f339aed64752df8e91b3274884
Rafi <mr.k779@outlook.com>
2025-12-30 03:52:22 -0500

    Second blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL("https://hackernoon.com/images/**")],
+  },
 };
 
 export default withMDX(config);
