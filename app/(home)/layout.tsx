939975f785fb2a8e9b06f9daa9827b1e9d2cb2c2
Rafi <mr.k779@outlook.com>
2026-01-21 23:38:22 -0500

    UI improvements

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import { Footer } from "@/components/footer";
 import { baseOptions } from "@/lib/layout.shared";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const options = baseOptions();
+
+  if (!pathname.startsWith("/blog")) {
+    options.searchToggle = {
+      enabled: false,
+    };
+  }
+
+  if (pathname !== "/about") {
+    delete options.githubUrl;
+  }
+
   return (
-    <HomeLayout {...baseOptions()}>
+    <HomeLayout {...options}>
       <div className="flex flex-col min-h-[calc(100vh-var(--fd-nav-height,3.5rem))]">
         <div className="flex-1">{children}</div>
         <Footer />
